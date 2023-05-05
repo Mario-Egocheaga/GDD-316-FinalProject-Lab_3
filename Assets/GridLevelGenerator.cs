84edd5678aec2b701f4f47247eaa1d3a4facccdc
Mario-Egocheaga <work.mario.egocheaga@gmail.com>
2023-05-05 13:54:43 -0400

    Minor Fixes + Implementation

--- Assets/GridLevelGenerator.cs
+++ Assets/GridLevelGenerator.cs
@@ -15,12 +15,13 @@ public class GridLevelGenerator : MonoBehaviour
 
     public float minPropDistance = 2f;  // Minimum distance between props
 
-    private bool ranY, ranX;
+    public int numWalls = 4;
 
     void Start()
     {
         parent = this.transform;
         GenerateLevel();
+        spawnInfrastructure();
         ChangeTransform();
     }
 
@@ -34,12 +35,13 @@ public class GridLevelGenerator : MonoBehaviour
                 // Loop through each layer of props
                 for (int layer = 0; layer < numLayers; layer++)
                 {
+                    /*
                     // Choose whether to place a prop in this cell and layer based on the prop density
                     if (Random.value < propDensity)
                     {
                         // Choose a random prop from the array
                         GameObject prop = props[Random.Range(0, props.Length)];
-
+                        
                         // Calculate the position of the prop within the cell and layer
                         Vector3 position = new Vector3((x - gridWidth / 2f + 0.5f) * cellSize, layer * cellSize, (y - gridHeight / 2f + 0.5f) * cellSize);
 
@@ -49,10 +51,40 @@ public class GridLevelGenerator : MonoBehaviour
                         propPlaced.transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y + Random.Range(0f, 360f), transform.rotation.z);
 
                     }
+                    */
                 }
             }
         }
     }
+
+    void spawnInfrastructure()
+    {
+        GameObject wall = wall = props[7];
+
+        float radius = 5;
+
+        Vector3 center = new Vector3(transform.position.x +  Random.Range(-8f, 8f), 0, transform.position.z + Random.Range(-8f, 8f));
+
+        for (int i = 0; i < numWalls; i++)
+        {
+            Vector3 pos = RanCircle(center, 2.0f);
+            Quaternion rot = Quaternion.FromToRotation(Vector3.forward, center-pos);
+
+            GameObject wallPlaced = Instantiate(wall, pos, rot);
+
+        }
+    }
+
+    Vector3 RanCircle(Vector3 center, float radius)
+    {
+        float ang = Random.value * 360;
+        Vector3 pos;
+        pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+        pos.y = center.y;
+        pos.z = center.z + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+        return pos;
+    }
+
 
     void ChangeTransform()
     {
